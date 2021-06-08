a439eb1e394ae86d8826baad5ec9127bad804403
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-06-08 14:21:14 +0200

    feat: use table instance to fill the table with data

--- src/components/BasicTable.js
+++ src/components/BasicTable.js
@@ -12,21 +12,46 @@ function BasicTable() {
         columns:columns,
         data:data,
     })
-    return (
-        <table>
-            <thread>
-                <tr>
-                    <th>
 
-                    </th>
-                </tr>
+    const 
+        { 
+            getTableProps,
+            getTableBodyProps,
+            headerGroups, 
+            rows, 
+            prepareRow 
+        } = tableInstance
+    return (
+        <table {...getTableProps()}>
+            <thread>
+                {
+                    headerGroups.map((headerGroup) =>(
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {
+                                headerGroup.headers.map(column => (
+                                    <th {...column.getHeaderGroupProps()}>
+                                        {column.render('Header')}
+                                    </th>
+                                ))
+                            }
+                        </tr>
+                    ))
+                }
             </thread>
-            <tbody>
-                <tr>
-                    <td>
-                        
-                    </td>
-                </tr>
+            <tbody {...getTableBodyProps()}>
+                {rows.map((row) => {
+                    prepareRow(row) 
+                    return(
+                        <tr {...row.getRowProps()}>
+                            {
+                                row.cells.map((cell) =>{
+                                    return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                                })
+                            }
+
+                        </tr>
+                    )
+                })}
             </tbody>
         </table>
     )
